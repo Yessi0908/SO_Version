17ab1b880a38ad6897aae206830f3af6d3586a39
juana <jramirezs26>
2024-08-07 01:14:22 -0600

    Agregar archivos de proyecto.

---
+++ Tarea03/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Tarea03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sistemas Operativos");
+
+        }
+    }
+}
